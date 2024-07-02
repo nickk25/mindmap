99ea19261ea7b5295daed43d0899d34f20fb8211
Nicolas Arambulo <nicolas@cloudnonic.com>
2024-07-03 01:44:08 +0200

    style: :lipstick: update node model

--- components/node-model.tsx
+++ components/node-model.tsx
@@ -1,38 +1,30 @@
 "use client"
 
 import React from "react"
-import { DragControls, Sphere } from "@react-three/drei"
-import { Container, Root, Text } from "@react-three/uikit"
-import * as THREE from "three"
+import { Text } from "@radix-ui/themes"
+import { DragControls, Html } from "@react-three/drei"
 
 export function NodeModel({
-  data,
+  name,
   position,
-  onPositionChange,
-  onClick,
+  rotation,
+  scale,
+  ...props
 }: {
-  data: string
+  name: string
   position: [number, number, number]
-  onPositionChange: (position: THREE.Vector3) => void
-  onClick: () => void
+  rotation?: [number, number, number]
+  scale?: number
+  props?: any
 }) {
-  const handleDrag = (e: { elements: number[] }) => {
-    const matrix = new THREE.Matrix4().fromArray(e.elements)
-    const vec = new THREE.Vector3().setFromMatrixPosition(matrix)
-    onPositionChange(vec)
-  }
-
   return (
-    <DragControls onDrag={handleDrag}>
-      <mesh position={position} onClick={onClick}>
-        <Sphere scale={0.5}>
-          <meshPhongMaterial wireframe={true} color="hotpink" opacity={0.3} />
-        </Sphere>
-        <Root anchorX="center" anchorY="center">
-          <Container>
-            <Text color="white">{data}</Text>
-          </Container>
-        </Root>
+    <DragControls>
+      <mesh {...props} position={position} rotation={rotation} scale={scale}>
+        <dodecahedronGeometry />
+        <meshStandardMaterial roughness={0.75} emissive="#404057" />
+        <Html distanceFactor={10}>
+          <Text>{name}</Text>
+        </Html>
       </mesh>
     </DragControls>
   )
