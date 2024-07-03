0557e99e772ae780e74e85e53b2b9c2772322501
Nicolas Arambulo <nicolas@cloudnonic.com>
2024-07-03 05:59:38 +0200

    feat: :sparkles: focus camera on node selected

--- components/selecto-to-zoom.tsx
+++ components/selecto-to-zoom.tsx
@@ -1,15 +1,46 @@
-import { useBounds } from "@react-three/drei"
+"use client"
+
+import { useEffect } from "react"
+import { Preload, useBounds } from "@react-three/drei"
+import { useThree } from "@react-three/fiber"
+
+import { useData } from "@/lib/contexts/useData"
 
 export function SelectToZoom({ children }: { children: React.ReactNode }) {
   const api = useBounds()
+  const { selectNode, selectedNodeId } = useData()
+  const state = useThree((state) => state.scene.children)
+
+  useEffect(() => {
+    const node = state
+      .find((node) => node.name === "")
+      ?.children[0]?.children?.find((node) => {
+        if (node?.children[0]?.name === selectedNodeId) {
+          return node?.children[0]
+        }
+      })
+
+    if (node) {
+      api.refresh(node).fit()
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedNodeId])
+
   return (
     <group
+      name="select-to-zoom"
       onClick={(e) => (
-        e.stopPropagation(), e.delta <= 2 && api.refresh(e.object).fit()
+        selectNode(e.object.name),
+        e.stopPropagation(),
+        e.delta <= 2 && api.refresh(e.object).fit()
       )}
-      onPointerMissed={(e) => e.button === 0 && api.refresh().fit()}
+      onPointerMissed={(e) => (
+        selectNode(null), e.button === 0 && api.refresh().fit()
+      )}
     >
       {children}
+      <Preload all />
     </group>
   )
 }
