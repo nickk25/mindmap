491fe2cfd5fb942dd417cd9bde9c550d196cc83d
Nicolas Arambulo <nicolas@cloudnonic.com>
2024-07-03 19:17:15 +0200

    feat: :sparkles: display panels in the ui

--- app/network/page.tsx
+++ app/network/page.tsx
@@ -1,11 +1,15 @@
+import { PanelChildren } from "@/components/panel-children"
+import { PanelList } from "@/components/panel-list"
 import { Flow } from "@/components/reactflow/flow"
 
 export default function Network() {
   return (
     <main
       id="network"
-      className="flex h-screen min-h-screen w-screen flex-col items-center justify-between"
+      className="relative flex h-screen min-h-screen w-screen flex-col items-center justify-between"
     >
+      <PanelList />
+      <PanelChildren />
       <Flow />
     </main>
   )
