21e1994148e531df1be6d8250257d85d9def7de4
Nicolas Arambulo <nicolas@cloudnonic.com>
2024-07-03 22:40:09 +0200

    feat: :sparkles: add clerk provider

--- lib/providers/providers.tsx
+++ lib/providers/providers.tsx
@@ -4,12 +4,15 @@ import { RadixProvider } from "@/lib/providers/radix-provider"
 import { ThemeProvider } from "@/lib/providers/theme-provider"
 
 import { DataProvider } from "../contexts/useData"
+import { CustomizedClerkProvider } from "./clerk-provider"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
       <RadixProvider>
-        <DataProvider>{children}</DataProvider>
+        <CustomizedClerkProvider>
+          <DataProvider>{children}</DataProvider>
+        </CustomizedClerkProvider>
       </RadixProvider>
     </ThemeProvider>
   )
